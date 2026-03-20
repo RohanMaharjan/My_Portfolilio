99fb1ffb2ca1410d3ed30ac58fea94afabf01ec9
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-20 14:37:15 +0545

    added navbar css

--- src/navbar.js
+++ src/navbar.js
@@ -1,20 +1,25 @@
 import React from "react";
-
+import "./navbar.css";
 
 //component declaration
 const Navbar = () => {
     return(
-        <div>
-            <h1>My Portfolio</h1>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#achievements">Achievements</a></li>
-                <li><a href="#contact">Contact Me</a></li>
-            </ul>
-        </div>
+        <nav className="navbar">
+            <div className="navbar-left">
+                <h1>My Portfolio</h1>
+            </div>
+
+            <div className="navbar-right">
+                    <ul className="nav-links">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#achievements">Achievements</a></li>
+                        <li><a href="#contact">Contact Me</a></li>
+                    </ul>
+            </div>
+        </nav>
     );
 };
 
