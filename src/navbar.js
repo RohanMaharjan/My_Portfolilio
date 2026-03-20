464f74d0e698c6a4502bb1159e8e14525732250f
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-20 20:20:56 +0545

    update navigation

--- src/navbar.js
+++ src/navbar.js
@@ -13,7 +13,6 @@ const Navbar = () => {
                     <ul className="nav-links">
                         <li><a href="#home">Home</a></li>
                         <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
                         <li><a href="#projects">Projects</a></li>
                         <li><a href="#achievements">Achievements</a></li>
                         <li><a href="#contact">Contact Me</a></li>
