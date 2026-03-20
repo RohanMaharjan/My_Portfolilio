dc562c228dbdc9f5a2c7b75c34d0c938317ab787
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-20 15:32:37 +0545

    added home page css

--- src/home.jsx
+++ src/home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import "./home.css";
 
 //component declaration for home
 const Home = () => {
     return (
-        <div>
+        <div className="home-container">
             <h1>I'M ROHAN MAHARJAN</h1>
         </div>
         
