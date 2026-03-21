36d37b3930e5239dee906867c7158eecf948892b
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-21 13:46:35 +0545

    added css to contact page

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPhone, faEnvelope, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import "./contact.css"
 
 const Contact = () => {
     return(
-        <div id="contact">
+        <div className="contact-container" id="contact">
             <h1>Contact Me</h1>
-            <h2>Get In Touch</h2>
-            <p>Feel free to get in touch with me if you have any questions or comments. <br />Thrilled to begin our professional journey together!</p>
-            <ul>
-                <li>
-                    <FontAwesomeIcon icon={faPhone} /><strong>Phone:</strong>+977-9742875914
-                </li>
-                <li>
-                    <FontAwesomeIcon icon={faEnvelope} /><strong>Email:</strong>rohanmhj9@gmail.com
-                </li>
-                <li>
-                    <FontAwesomeIcon icon={faMapMarkerAlt} /><strong>Location:</strong>Harisiddhi-28, Lalitpur
-                </li>
-            </ul>
-            <div>
+            <div className="contact-left">
+                <h2>Get In Touch</h2>
+                <p>Feel free to get in touch with me if you have any questions or comments. <br />Thrilled to begin our professional journey together!</p>
+                <ul>
+                    <li>
+                        <FontAwesomeIcon icon={faPhone} /><strong>Phone:</strong>+977-9742875914
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faEnvelope} /><strong>Email:</strong>rohanmhj9@gmail.com
+                    </li>
+                    <li>
+                        <FontAwesomeIcon icon={faMapMarkerAlt} /><strong>Location:</strong>Harisiddhi-28, Lalitpur
+                    </li>
+                </ul>
+            </div>
+
+            <div className="contact-right">
                 <h2>Message Me</h2>
                 <form>
                     <input type="text" placeholder="Enter Name" required />
