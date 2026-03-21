c6e7ad75036c997fbf8d172f003af761b8eac06e
Rohan Maharjan <rohanmhj9@gmail.com>
2026-03-21 10:51:34 +0545

    removed clickable icons and learining section

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./navbar";
 import Home from "./home";
 import About from "./about";
 import Technologies from "./technologies";
-import PythonPage from "./python";
 
-function MainPage() {
+function App() {
   return (
-    <>
+    <div>
       <Navbar />
       <Home />
       <About />
       <Technologies />
-    </>
-  );
-}
-
-function App() {
-  return(
-    <div>
-      <Router>
-
-        <Routes>
-          {/* main portfolio page(scrolling) */}
-          <Route path="/" element={<MainPage />} />
-
-          <Route path="/python" element={<PythonPage />} />
-        </Routes>
-      </Router>
     </div>
   );
-
 }
+
+
 
 export default App;
--- src/technologies.jsx
+++ src/technologies.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import "./technologies.css";
-import { Link } from "react-router-dom";
+import "./technologies.css"
 
 import pythonLogo from "./assets/Python_logo.jpeg";
-import djangoLogo from "./assets/django_logo.png";
+import djangoLogo from "./assets/django_logo.png"
 import reactLogo from "./assets/React_logo.png";
 import nodeLogo from "./assets/node_logo.png";
 import gitLogo from "./assets/git_logo.png";
@@ -13,66 +12,49 @@ import postgresLogo from "./assets/postgres_logo.png";
 import mongoLogo from "./assets/mongo_logo.png";
 
 const Technologies = () => {
-    return (
+    return(
         <div className="tech-container" id="tech">
             <h2>Technologies</h2>
             <div className="tech-grid">
                 <div className="tech-item">
-                    <Link to="/python">
-                        <img src={pythonLogo} alt="Python" />
-                    </Link>
+                            <img src={pythonLogo} alt="Python" />
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/django">
                         <img src={djangoLogo} alt="Django" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/react">
                         <img src={reactLogo} alt="React" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/node">
                         <img src={nodeLogo} alt="Node" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/git">
                         <img src={gitLogo} alt="Git" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/c">
                         <img src={cLogo} alt="C" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/cplusplus">
                         <img src={cplusLogo} alt="C++" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/postgres">
                         <img src={postgresLogo} alt="PostgreSQL" />
-                    </Link>
                 </div>
-
+                    
                 <div className="tech-item">
-                    <Link to="/mongo">
                         <img src={mongoLogo} alt="MongoDB" />
-                    </Link>
                 </div>
             </div>
         </div>
-    );
-};
+
+    )
+}
 
 export default Technologies;
